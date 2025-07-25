11c4fccb043690aaefe50bed1e74ee21a056dc7a
pika0131 <129724220+pika0131@users.noreply.github.com>
2025-07-26 04:07:32 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -7,5 +7,5 @@ function getClock(){
     const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = (`${hours}:${minutes}:${seconds}`);
 }
-getClock();
+getClock(); 123
 setInterval(getClock, 1000);
